ca4cc0df02a0bf1d7686219b3801a6656ccba79d
K. <kylebsmall32@gmail.com>
2025-04-14 17:23:58 -0400

    change import

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/KdotIV/NostalgiaCade/NostalgiaCadeCLI/cmd"
+import "github.com/KdotIV/NostalgiaCade/cmd"
 
 func main() {
 	cmd.Execute()
